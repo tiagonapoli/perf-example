1e3bb2b95dcd530cb2d497e0c444b5883ba5f34e
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2022-03-31 20:07:48 -0300

    upd
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- app-prof/profile.sh
+++ app-prof/profile.sh
@@ -1,6 +1,15 @@
 #! /bin/bash
-g++ -O0 -fno-inline-small-functions -fno-omit-frame-pointer main.cpp 
+g++ -O0 -fno-inline-small-functions -fno-omit-frame-pointer main.cpp
+
+# -F 1000: 1000Hz
+# -g: Gravar os stack traces 
 perf record -F 1000 -g --call-graph dwarf ./a.out
+
+# gera o arquivo com os samples da stacks
 perf script > /tmp/out
+
+# parseia o arquivo com os samples das stacks para um formato compativel pra gerar o flamegraph
 ./stackcollapse-perf.pl /tmp/out > /tmp/collapsed
+
+# gera o flamegraph
 ./flamegraph.pl /tmp/collapsed > flamegraph.svg
